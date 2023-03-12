f8066592e11b11c878dc5ac75353dc41e2be5ed4
Prashant Bhatt <114800910+Prashant-Bhatt-2000@users.noreply.github.com>
2023-03-12 18:28:09 +0530

    Add files via upload

---
+++ Password Generator/Password_generator.py
@@ -0,0 +1,27 @@
+import random
+
+characters = '~`!@#$%^^*()_+=/*-+.,""1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+while True: 
+    passwordChar = int(input("Enter the no of characters you want in your password : "))
+
+    if passwordChar < 7: 
+        print("Your password will not be strong. Enter the no greater than 7")
+
+
+    if passwordChar >= 8:
+        password = ''
+        for len in range(passwordChar):
+            random_char = random.choice(characters)
+            password += random_char
+        
+    # print(password)
+    list_pass = list(password)
+    random.shuffle(list_pass)
+    password_generated = ''.join(list_pass)
+
+    with open('created_password.txt', 'w') as create: 
+        password = 'your generated password : '
+        create.write(password)
+        create.write(password_generated)
+    print(password_generated)
